f98b6069f235a746e2f46e4562ab222e8a338627
Jonatas Rios <jonatasguimaraesg@gmail.com>
2025-07-29 14:52:09 -0300

    k

--- Aula011/ex008.js
+++ Aula011/ex008.js
@@ -1,6 +1,6 @@
-var velocidade = 5
+var velocidade = 10
 console.log(`A velocidade do carro é ${velocidade}Km/h`)
-if (velocidade > 60){
+if (velocidade > 60){//condição simples
     console.log("Você passou do limite voce esta multado!!")
 }
 console.log("Digija com cinto de segurança")
---
+++ Aula012/ex011.js
@@ -0,0 +1,10 @@
+var idade = 22
+if (idade < 16){
+    console.log("Nao vota")
+}
+else{
+    if (idade >= 16 && idade < 18){
+        console.log("Voto opcional")
+    }
+    
+}
