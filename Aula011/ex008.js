c2857b1f2cd762ba427a65a74cb34969a6408bfe
Jonatas Rios <jonatasguimaraesg@gmail.com>
2025-07-29 13:03:30 -0300

    k

--- Aula011/ex008.js
+++ Aula011/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou!')
+var velocidade = 5
+console.log(`A velocidade do carro é ${velocidade}Km/h`)
+if (velocidade > 60){
+    console.log("Você passou do limite voce esta multado!!")
+}
+console.log("Digija com cinto de segurança")
